c32152decdf8e296f1a8ec576c717787a73ff135
Einar Júlíusson <eju@mh.is>
2019-11-27 16:54:45 +0000

    Með lagfæringum

--- src/index.js
+++ src/index.js
@@ -24,13 +24,86 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < content.length; i += 1) {
       console.log(content[i].type);
       console.log(content[i].data);
-      const lectEl = document.getElementsByClassName('lecture')[0];
-      const lect = document.createElement('lecture__content');
-      lect.classList.add(content[i].type);
-      lect.setAttribute('type', content[i].type);
-      lect.textContent = content[i].data;
-      lectEl.appendChild(lect);
+      if (content[i].type === 'youtube') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const lect = document.createElement('a');
+        lect.href = content[i].data;
+        lect.classList.add(content[i].type);
+        lect.setAttribute('type', content[i].type);
+        lect.textContent = "Myndband";
+        lectEl.appendChild(lect);
+      }
+
+      if (content[i].type === 'text') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const lect = document.createElement('div');
+        lect.classList.add(content[i].type);
+        lect.setAttribute('type', content[i].type);
+        lect.textContent = content[i].data;
+        lectEl.appendChild(lect);
+      }
+      
+      if (content[i].type === 'image') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const contain = document.createElement('div');
+        contain.classList.add('figure');
+        lectEl.appendChild(contain);
+        const lect = document.createElement('img');
+        lect.classList.add(content[i].type);
+        lect.setAttribute('src', content[i].data);
+        contain.appendChild(lect);
+        const capt = document.createElement('figcaption');
+        capt.textContent = content[i].caption;
+        contain.appendChild(capt);
+      }
+
+      if (content[i].type === 'quote') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const contain = document.createElement('div');
+        contain.classList.add('quote');
+        lectEl.append(contain);
+        const lect = document.createElement('div');
+        lect.classList.add(content[i].type);
+        lect.textContent = content[i].data;
+        contain.appendChild(lect);
+        /*
+        const att = document.createElement('div');
+        att.textContent = content[i].attribute;
+        contain.textContent(att);
+        */
+      }
+
+      if (content[i].type === 'heading') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const lect = document.createElement('div');
+        lect.classList.add(content[i].type);
+        lect.setAttribute('type', content[i].type);
+        lect.textContent = content[i].data;
+        lectEl.appendChild(lect);
+      }
+
+      if (content[i].type === 'list') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const lect = document.createElement('ul');
+        lect.classList.add(content[i].type);
+        lect.setAttribute('type', content[i].type);
+        lect.textContent = content[i].data;
+        lectEl.appendChild(lect);
+      }
+
+      if (content[i].type === 'code') {
+        const lectEl = document.getElementsByClassName('lecture')[0];
+        const lect = document.createElement('div');
+        lect.classList.add(content[i].type);
+        lect.setAttribute('type', content[i].type);
+        lect.textContent = content[i].data;
+        lectEl.appendChild(lect);
+      }
     }
+  }
+
+  function endLecture(e) {
+    e.target.parentNode.classList.toggle('item--done');
   }
 
   function load() {
@@ -59,6 +132,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (isLecturePage) {
     load();
+    const check = document.querySelector('.finish');
+    check.addEventListener('click', endLecture);
   } else {
     const list = new List();
     list.load();
